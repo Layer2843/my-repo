f74f70ad0892fa119134f411df73b33c0c4ecbc6
Adrian Kustra <kustraa@tlimc.local>
2025-11-07 13:37:33 +0100

    JavaSkrypt

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,47 +1,22 @@
-const Header = (props) => {
-  return(
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-    <p>{props.name} {props.number}</p>
-    </>
-  )
-}
-
-const Content = () => {
-  return (
-    <div>
-      <Part name="Fundamentals of React" number="10"/>
-      <Part name="Using props to pass data" number="7"/>
-      <Part name="State of a component" number="14"/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <p>Number of exercises {props.total}</p>
-  )
-}
-
-
 const App = () => {
   const course = 'Half Stack application development'
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
-      <Header course={course}/>
-
-      <Content/>
-
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <h1>{course}</h1>
+      <p>{part1.name} {part1.exercises}</p>
     </div>
   )
 }
