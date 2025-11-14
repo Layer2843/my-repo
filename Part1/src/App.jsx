07b342c463eddd28164e96e56c3da7f9232ce867
Adrian Kustra <kustraa@tlimc.local>
2025-11-14 12:40:49 +0100

    courseinfo 1.5

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -8,7 +8,7 @@ const Header = ({course}) => {
 const Content = ({course}) => {
   return (
     <div>
-      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+      <p>{course.parts[0].name} {course.parts[0].exercises}!</p>
       <p>{course.parts[1].name} {course.parts[1].exercises}</p>
       <p>{course.parts[2].name} {course.parts[2].exercises}</p>
     </div>
