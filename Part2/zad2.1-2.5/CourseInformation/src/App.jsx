32fbcab70837b5a485e0eb7f3e87d9bc97553ac4
Adrian Kustra <kustraa@tlimc.local>
2026-01-15 13:18:35 +0100

    courseinfo 2.4

--- Part2/zad2.1-2.5/CourseInformation/src/App.jsx
+++ Part2/zad2.1-2.5/CourseInformation/src/App.jsx
@@ -1,38 +1,74 @@
-import Course from './components/Course'
+import React from 'react'
 
 const App = () => {
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-      
-    ]
-  }
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
 
-  return <Course course={course} />
+      {course.map(course => {
+        const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+        return (
+          <div key={course.id}>
+            <h2>{course.name}</h2>
+
+            {course.parts.map(content => (
+              <p key={content.id}>
+                {content.name} {content.exercises}
+              </p>
+            ))}
+
+            <b>total exercises {total}</b>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default App
-
-
